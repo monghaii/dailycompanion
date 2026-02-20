0c30e1042362d749572c776aa81834d4e122c4ac
Matt <mattonghai@gmail.com>
2026-02-19 16:03:32 -0800

    ran automatic save at 2026-02-19 16:03:32 PST

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,6 @@ const nextConfig = {
         source: "/ingest/:path*",
         destination: "https://us.i.posthog.com/:path*",
       },
-      {
-        source: "/ingest/decide",
-        destination: "https://us.i.posthog.com/decide",
-      },
     ];
   },
   skipTrailingSlashRedirect: true,
