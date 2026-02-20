f357c125507efcd29507f25a53337f38bfc50dda
Matt <mattonghai@gmail.com>
2026-02-19 16:04:45 -0800

    ran automatic save at 2026-02-19 16:04:45 PST

--- src/components/PostHogProvider.js
+++ src/components/PostHogProvider.js
@@ -11,7 +11,7 @@ if (
   !posthog.__loaded
 ) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: "/ingest",
+    api_host: "/a",
     ui_host: "https://us.posthog.com",
     capture_pageview: false,
     capture_pageleave: true,
